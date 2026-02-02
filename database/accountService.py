9eef7576e2f3b24e0272c4f0596ca76ccdf3ddf6
Christian Fränken <christian.f21@gso.schule.koeln>
2026-02-02 11:08:22 +0100

    Added AccountService

--- database/accountService.py
+++ database/accountService.py
@@ -1,2 +1,23 @@
-from sqlalchemy import Engine
+from database.model.accountmodel import AccountModel
 
+
+class AccountService:
+    def __init__(self, db):
+        self.db = db
+
+    def createAccount(self, account):
+        with self.db.session as session:
+            session.add(account)
+            session.commit()
+
+    def editAccount(self, accountId, dict):
+        with self.db.session as session:
+            account = AccountModel.query.get(accountId)
+            account.update(dict)
+            session.commit()
+
+    def deleteAccount(self, accountId):
+        with self.db.session as session:
+            account = AccountModel.query.get(accountId)
+            session.delete(account)
+            session.commit()
