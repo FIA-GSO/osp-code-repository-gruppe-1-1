92aa489e079569cac86d5ab073103fe8fd0d3ee2
Christian Fränken <christian.f21@gso.schule.koeln>
2026-02-02 14:08:03 +0100

    Refactor AccountService

--- database/accountService.py
+++ database/accountService.py
@@ -1,27 +1,25 @@
 from sqlalchemy.orm import Session
 from database.model.accountModel import AccountModel
+from database.model.base import db
 
 
-class AccountService:
-    def __init__(self, engine):
-        self.engine = engine
+def create_account(account):
+    with Session(db.engine) as session:
+        session.add(account)
+        session.commit()
+        session.refresh(account)
+        return account
 
-    def create_account(self, account):
-        with Session(self.engine) as session:
 
-            session.add(account)
-            session.commit()
-            session.refresh(account)
-            return account
+def edit_account(account_id, dict):
+    with Session(db.engine) as session:
+        account = AccountModel.query.get(account_id)
+        account.update(dict)
+        session.commit()
 
-    def edit_account(self, account_id, dict):
-        with Session(self.engine) as session:
-            account = AccountModel.query.get(account_id)
-            account.update(dict)
-            session.commit()
 
-    def delete_account(self, account_id):
-        with Session(self.engine) as session:
-            account = AccountModel.query.get(account_id)
-            session.delete(account)
-            session.commit()
+def delete_account(account_id):
+    with Session(db.engine) as session:
+        account = AccountModel.query.get(account_id)
+        session.delete(account)
+        session.commit()
