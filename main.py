cade6155aabff8d5c64308659ed701ff0d67335b
Christian Fränken <christian.f21@gso.schule.koeln>
2026-02-02 10:53:06 +0100

    Add main starter

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import *
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(
+
+)
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
