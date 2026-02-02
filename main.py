29994d8847a1b92b526c0d5ec9e1ce350898d167
Marcel Sorges <marcel.sorges@trimexa.de>
2026-02-02 11:01:22 +0100

    adding bootstrap

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, session, redirect, url_for, render_template, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = b'dlajwasdhdddqwf98fg9f23803f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(
@@ -10,4 +11,14 @@ db = SQLAlchemy(
 db.init_app(app)
 with app.app_context():
     db.create_all()
+
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=4000, debug=True)
+
 
